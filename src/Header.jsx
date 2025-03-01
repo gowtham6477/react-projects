14bc897a08298dc3bcc403b3fcb1ba58884d5b24
gowtham6477 <jgjg6477@gmail.com>
2025-03-01 15:54:47 +0530

    first react project

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="src/assets/react-icon.png" className="react-img" alt="React logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-item">Pricing</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
